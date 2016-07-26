bdabe083e4e1d029f324df29fae1d536be4eaf91
Eric Manda <eric.manda@gmail.com>
2016-07-25 19:04:39 -0600

    Added user input to choose subject

--- filter_and_epoch.py
+++ filter_and_epoch.py
@@ -6,9 +6,9 @@ from mne import find_events, Epochs
 
 # 'runs' selects which versions of the experiment to epoch. Available
 # choices are: [3, 7, 11], [4, 8, 12], [5, 9, 13], and [6, 10, 14]
-baseline = (None, 0)
-begin = 1
-end = 110
+baseline = None
+begin = int(input('Enter first subject: '))
+end = int(input('Enter last subject: '))
 event_id = dict(left_fist=2, right_fist=3)
 runs = [3, 7, 11]
 tmax = 0.5
@@ -24,6 +24,6 @@ for num in range(begin, end):
         # with iir_params=None or left out.
         raw.filter(7.0, 30.0, method='iir', n_jobs=2)
         events = find_events(raw, consecutive=False)
-        epochs = Epochs(raw, events, event_id, tmin, tmax, proj=True,
+        epochs = Epochs(raw, events, event_id, tmin, tmax, proj=False,
                         picks=None, baseline=baseline, preload=True)
         epochs.save('{}-epo.fif'.format(raw.info['filename'][:-4]))
