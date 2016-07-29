bf2b3138880c16f35300f6ee9636b202729b4455
Eric Manda <eric.manda@gmail.com>
2016-07-29 08:07:55 -0600

    Added cpu_count for portability

--- filter_and_epoch.py
+++ filter_and_epoch.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-from prepare_raw import add_montage, fix_channels, load_subject
 from mne import find_events, Epochs
 from mne.preprocessing import ICA
-from os.path import splitext
+from multiprocessing import cpu_count
 from numpy.random import RandomState
+from os.path import splitext
+from prepare_raw import add_montage, fix_channels, load_subject
 
 
 # 'runs' selects which versions of the experiment to epoch. Available
@@ -14,6 +15,7 @@ baseline = None
 begin = int(input('Enter first subject: '))
 end = int(input('Enter last subject: '))
 event_id = dict(left_fist=2, right_fist=3)
+n_cores = cpu_count()
 random_state = RandomState(42)
 runs = [3, 7, 11]
 tmax = 0.5
@@ -32,7 +34,7 @@ for num in range(begin, end):
         # Band-pass filter to capture the relevant signal (alpha and
         # beta ranges). Butterworth filter is implied by method='iir'
         # with iir_params=None or left out.
-        raw.filter(7.0, 30.0, method='iir', n_jobs=2)
+        raw.filter(7.0, 30.0, method='iir', n_jobs=n_cores)
         ica = ICA(n_components=0.95, random_state=random_state)
         ica.fit(raw, decim=3)
         ica.apply(raw)
