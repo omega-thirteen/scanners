ec0fe21b489f1083d959846abeac8fc68eacd518
Eric Manda <eric.manda@gmail.com>
2016-08-01 02:17:49 -0600

    Reverted to previous commit

--- write_hdf5.py
+++ write_hdf5.py
@@ -1,18 +1,15 @@
 from glob import glob
 from mne import read_epochs, read_evokeds
-from mne.decoding import EpochsVectorizer
+
 
 # The following block collects all epochs into an HDF5 file:
 
 epoch_files = sorted(glob('**/*-epo.fif', recursive=True))
 
 for f in epoch_files:
-    epo = read_epochs(f,
-                      proj=False,
-                      preload=True)
-    vectorizer = EpochsVectorizer(epo.info)
-    epo = vectorizer.fit_transform(epo)
-    df = epo.to_data_frame(index=None)
+    df = read_epochs(f,
+                     proj=False,
+                     preload=True).to_data_frame(index=None)
     df.rename(columns={'STI 014': 'event'}, inplace=True)
     df.to_hdf('data/misc/epochs.h5',
               'epochs',
