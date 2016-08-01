f4487b2b459da2fdbe9a06748fec08c5f2b7c6b8
Eric Manda <eric.manda@gmail.com>
2016-08-01 00:24:27 -0600

    Tuned SGDClassifier and added RandomizedSearchCV

--- sgd_classifier.py
+++ sgd_classifier.py
@@ -1,7 +1,7 @@
-from sklearn.cross_validation import train_test_split, cross_val_score
+from multiprocessing import cpu_count
 from sklearn.decomposition import FastICA
-from sklearn.grid_search import GridSearchCV
-from sklearn.cross_validation import train_test_split
+from scipy.stats import lognorm
+from scipy.stats import randint
 from sklearn.linear_model import SGDClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
@@ -10,6 +10,7 @@ import numpy as np
 
 
 random_state = np.random.RandomState(42)
+n_cores = cpu_count()
 
 X_train, X_test, y_train, y_test = train_test_split(X,
                                                     y,
@@ -21,24 +22,24 @@ scaler.fit(X_train)
 X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)
 pipe = Pipeline(estimators)
-
-# When loss='hinge', SGDClassifier becomes a linear SVM.
-# When loss='log', SGDClassifier becomes a logistic regression.
-
-# clf = SGDClassifier(alpha=0.0001,
-#                     average=False,
-#                     class_weight=None,
-#                     epsilon=0.1,
-#                     eta0=0.0,
+sgd = SGDClassifier(average=True,
+                    class_weight=None,
+                    fit_intercept=False,
+                    learning_rate='optimal',
+                    loss='hinge',
+                    n_jobs=n_cores,
+                    penalty='l2',
+                    random_state=random_state,
+                    verbose=0)
 #                     fit_intercept=True,
-#                     l1_ratio=0.15,
-#                     learning_rate='optimal',
-#                     loss='hinge',
-#                     n_iter=1,
-#                     n_jobs=2,
-#                     penalty='l2',
-#                     power_t=0.5,
-#                     random_state=random_state,
-#                     shuffle=True,
-#                     verbose=0,
+param_dist = {'alpha': lognorm(s=2, scale=np.exp(-4)),
+              'n_iter': randint(5, 26),
+              'shuffle': [True, False]}
+
+# run randomized search
+
+n_iter_search = 20
+random_search = RandomizedSearchCV(sgd,
+                                   param_distributions=param_dist,
+                                   n_iter=n_iter_search)
 #                     warm_start=False)
