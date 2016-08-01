abae7deca05b6a0aea1202ee68b62f4440689b48
Eric Manda <eric.manda@gmail.com>
2016-08-01 00:20:14 -0600

    Added train_test_split and StandardScaler functionality

--- sgd_classifier.py
+++ sgd_classifier.py
@@ -1,6 +1,7 @@
 from sklearn.cross_validation import train_test_split, cross_val_score
 from sklearn.decomposition import FastICA
 from sklearn.grid_search import GridSearchCV
+from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import SGDClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
@@ -10,14 +11,15 @@ import numpy as np
 
 random_state = np.random.RandomState(42)
 
+X_train, X_test, y_train, y_test = train_test_split(X,
+                                                    y,
+                                                    test_size=0.2,
+                                                    random_state=random_state)
+
 scaler = StandardScaler()
-ica = FastICA()
-sgd = SGDClassifier()
-
-estimators = [('scaler', StandardScaler()),
-              ('ica', FastICA()),
-              ('sgd', SGDClassifier())]
-
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
 pipe = Pipeline(estimators)
 
 # When loss='hinge', SGDClassifier becomes a linear SVM.
